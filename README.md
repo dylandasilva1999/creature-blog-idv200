238ce0b7cc4a6243adba2ddb490e2967c9fd79e2
DylanV190160 <190160@virtualwindow.co.za>
2020-10-13 20:29:33 +0200

    Implemented MariaDB. Update UserProfile.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # creature-blog-idv200
 
-##Basic Blog built with php and symfony.
+##Basic Blog built with php and symfony 4.
+
+
+Run http-server: >php -S localhost:8000 -t public
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,11 +1,13 @@
 <?php
-
     // src/Controller/ProfileController.php
     namespace App\Controller;
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\Routing\Annotation\Route;
-    use App\Entity\User;
+
+    use App\Entity\UserProfile;
+
     class ProfileController extends AbstractController {
         /**
          * @Route("/profile/{id}", name="profile_view")
@@ -14,38 +16,37 @@
 
             $userId = (int) $id;
                 
-            $mark = new User();
-            $mark->setId(1);
-            $mark->setName("Mark");
+            // $mark = new User();
+            // $mark->setId(1);
+            // $mark->setName("Mark");
                 
-            $grace = new User();
-            $grace->setId(2);
-            $grace->setName("Grace");
+            // $grace = new User();
+            // $grace->setId(2);
+            // $grace->setName("Grace");
                 
-            $bill = new User();
-            $bill->setId(3);
-            $bill->setName("Bill");
+            // $bill = new User();
+            // $bill->setId(3);
+            // $bill->setName("Bill");
                 
-            $dennis = new User();
-            $dennis->setId(4);
-            $dennis->setName("Dennis");
+            // $dennis = new User();
+            // $dennis->setId(4);
+            // $dennis->setName("Dennis");
 
-            $mark->addFriend($grace);
-            $mark->addFriend($bill);
-            $grace->addFriend($mark);
-            $bill->addFriend($mark);
-                
-            $users = [$mark, $grace, $bill, $dennis];
+            // $mark->addFriend($grace);
+            // $mark->addFriend($bill);
+            // $grace->addFriend($mark);
+            // $bill->addFriend($mark);
 
-            $model = array();
-            
+            $user = $this->getDoctrine()->getRepository(UserProfile::class)->find($userId);
+            // $users = [$mark, $grace, $bill, $dennis];
+
+            $model = array('user' => $user);
             $view = 'profile.html.twig';
-            foreach ($users as $user) {
-                if ($userId === $user->getId()) {
-                $model['user'] = $user;
-                }
-            }
-
+            // foreach ($users as $user) {
+            //     if ($userId === $user->getId()) {
+            //     $model['user'] = $user;
+            //     }
+            // }
             return $this->render($view, $model);
         }
     }

78e9bc24f6b20ac9292339c5cd38b9d988b62784
DylanV190160 <190160@virtualwindow.co.za>
2020-11-13 13:45:27 +0200

    PhpUnit Testing added

---
+++ tests/Entity/Tests/UserProfileTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\UserProfileTest;
+
+class UserProfileTest extends UserProfileTest {
+
+    public function testUsernameWorking () {
+        
+        $this->assertEquals('username');
+    }
+}
+
+?>
